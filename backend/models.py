44e74e8e5dd4908c81a433d87a081c312874ce51
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-07-07 17:50:12 -0300

    feat(models.py): adiciona schemas Pydantic (PartidaBase, PartidaCreate, PartidaRead)

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
-from annotated_types import Timezone
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field
 from sqlalchemy import Column, Enum, Integer, String, DateTime
 from sqlalchemy.sql import func
 import enum
@@ -26,3 +27,21 @@ class Partida(Base):
     atualizacao = Column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
+
+
+class PartidaBase(BaseModel):
+    time_a: str = Field(..., min_length=1)
+    time_b: str = Field(..., min_length=1)
+
+
+class PartidaCreate(PartidaBase):
+    pass
+
+
+class PartidaRead(PartidaBase):
+    id: int
+    placar_time_a: int
+    placar_time_b: int
+    tempo: PeriodoEnum
+    atualizacao: datetime
+    model_config = ConfigDict(from_attributes=True)
