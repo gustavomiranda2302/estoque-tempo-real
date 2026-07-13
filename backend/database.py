e775d8a9d7ec98109e32b9f1f1169f107a4ad7bb
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-07-13 17:33:31 -0300

    feat: configuracao do banco

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+import os
+from unittest.mock import Base
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://localhost/partida_db")
+
+ASYNC_DATABASE_URL = os.getenv(
+    "ASYNC_DATABASE_URL", "postgresql+asyncpg://localhost/partida_db"
+)
+
+engine = create_engine(DATABASE_URL)
+SessaoLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+async_engine = create_async_engine(ASYNC_DATABASE_URL)
+async_session = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
